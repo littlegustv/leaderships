6fb7559831ee62dd19c4c3fa079e36365b01c5f1
Benny Heller <echo29x@gmail.com>
2018-08-14 17:35:49 -0400

    progress on multiplayer

---
+++ server/Main.hx
@@ -0,0 +1,47 @@
+package ;
+
+typedef PlayerData = {
+  x: Int,
+  y: Int,
+  dir: Float,
+  speed: Float,
+  id: String
+};
+
+
+class Main {
+  var server:mphx.server.impl.Server;
+  public function new ()
+  {
+    server = new mphx.server.impl.Server("127.0.0.1",8000);
+
+    server.events.on("Join", function(data:Dynamic,sender:mphx.connection.IConnection)
+    {
+      trace("Player: "+data.id+" has joined!");
+
+      sender.data = {
+        x: data.x,
+        y: data.y,
+        dir: data.dir,
+        id: data.id,
+        speed: data.speed
+      };
+      server.broadcast("Join",data);
+    });
+    server.events.on("Update",function(data:Dynamic,sender:mphx.connection.IConnection)
+    {
+      //trace('Update data received on server', data);
+      sender.data = data;
+      server.broadcast("Update",data);
+    });
+    server.events.on("Shoot",function(data:Dynamic,sender:mphx.connection.IConnection)
+    {
+      server.broadcast("NB",data);
+    });
+    server.start();
+  }
+  public static function main ()
+  {
+    new Main();
+  }
+}
